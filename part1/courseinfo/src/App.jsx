aaf66d8ad0b530c9be3f132ef8d6ebdaf64fdab8
holat <habeebabatunde9@gmail.com>
2023-12-25 21:36:19 +0100

    course info step 2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -40,11 +40,11 @@ const App = () => {
 
   return (
     <div>
-      <Header course = {course} ></ Header>
-      <Content part = {part1} exercises = {exercises1} ></ Content>
-      <Content part = {part2} exercises = {exercises2}></ Content>
-      <Content part = {part3} exercises = {exercises3} ></ Content>
-      <Total total = {exercises1 + exercises2 + exercises3} ></ Total>
+      <Header course = {course} />
+      <Content part = {part1} exercises = {exercises1} />
+      <Content part = {part2} exercises = {exercises2} />
+      <Content part = {part3} exercises = {exercises3} />
+      <Total total = {exercises1 + exercises2 + exercises3} />
     </div>
   )
 }
